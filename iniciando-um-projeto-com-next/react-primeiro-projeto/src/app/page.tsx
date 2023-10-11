62a9a3f6f5f892399c4531dcfdbad796a0af9713
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-10-11 17:57:15 -0300

    import do import

--- iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
+++ iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
@@ -1,5 +1,6 @@
 // npm run dev, para start
-import {Circle, Square} from "@/components/geo";
+import { GeoForm } from "@/components/GeoForm";
+
 
 function Page(){
   return (
@@ -7,8 +8,7 @@ function Page(){
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>outro texto</h3>
 
-      <Square/>
-      <Circle/>
+      <GeoForm />
     </div>
   );
 }
