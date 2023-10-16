82167f7ef5de87723e4a3f016cd3ce4781beb0cd
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-10-16 19:20:15 -0300

    estados e eventos

--- iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
+++ iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
@@ -1,29 +1,20 @@
 // npm run dev, para start
 
+import { StudentTable } from "@/components/StudentTable";
+import { students } from "@/data/students";
+
 
 
 const Page = () =>{
 
-  const fullTime = new Intl.DateTimeFormat('pt-BR',{
-    timeStyle: 'short',
-  hour12: false
-}).format();
-
-const hour = new Date().getHours();
-let greeting = '';
-
-
-  
-  return(
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-     <div className="text-9xl font-bold">{fullTime}</div>
-     <div className="text-5xl font-bold">
-      {hour >= 0 && hour < 12 && 'Bom Dia'}
-      {hour >=12 && hour < 18 && 'Boa Tarde'}
-      {hour >=18 && hour < 23 && 'Boa Noite'}
-     </div>
-    </div>
+  return (
+    <div>
+      
+     
+     <StudentTable students={students}/>
+  </div>
   )
+  
 }
 
 export default Page;
