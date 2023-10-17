6c255b16a861c8045d3521227f38062d76bf7a0b
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-10-17 20:53:59 -0300

    .

--- iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
+++ iniciando-um-projeto-com-next/react-primeiro-projeto/src/app/page.tsx
@@ -1,23 +1,26 @@
 // npm run dev, para start
 //"use client" para eventos de click
-
 "use client"
 
+import { FormEvent, useState } from "react";
 
 const Page = () =>{
- 
-  const handleButtonClick =()=>{
-    alert('funcionou 4')
+
+  const [showSecret, setShowSecret] = useState(false);
+
+  const handlerClickButton =()=>{
+    setShowSecret(!showSecret);
   }
 
   return (
-    <div className="container w-screen h-screen flex justify-center items-center">
-      <button onClick={handleButtonClick}className="p-3 bg-blue-700 text-white rounded-md">Clique aqui</button>
-     
-     
+    <div className="container w-screen h-screen  flex-col flex justify-center items-center">
+      <button className="p-3 text-white bg-blue-700 rounded-md mr-3" onClick={handlerClickButton}>{showSecret ? 'ocultar' : 'mostrar'}</button>
+      {showSecret &&
+      <div className="p-3 bg-blue-600 rounded-md mt-3">area secreta</div>
+    }
   </div>
   )
-  
 }
+
 
 export default Page;
