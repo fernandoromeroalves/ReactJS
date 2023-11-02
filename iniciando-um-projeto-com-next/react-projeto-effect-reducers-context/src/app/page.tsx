62e1e69010846c50552a665dbb28cb13ff288d30
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-02 02:54:24 -0300

    .

--- iniciando-um-projeto-com-next/react-projeto-effect-reducers-context/src/app/page.tsx
+++ iniciando-um-projeto-com-next/react-projeto-effect-reducers-context/src/app/page.tsx
@@ -1,17 +1,38 @@
 // npm run dev, para start
 //"use client" para eventos de click
+ /*
+  useEffect()
+  1.definição da função que vai rodar.
+  2.definição de quando vai rodar.
+  3.definição do que fazer quando o componente sumir/unpload
+  */
 "use client"
+
+import { useEffect, useState } from "react";
 
 
 
 const Page = () => {
   
+  const [name, setName] = useState('fernando');
+  const [age, setAge] = useState(37)
+  
+  useEffect(()=>{
+    console.log('rodou o effect')
+  }, [name]);
+
+ 
 
   return ( 
     <div className="">
-     
+      <p>Meu Nome é {name} e tenho {age} anos</p>
 
+      <button className=" rounded-md bg-blue-700 border border-solid mt-5 p-2 mr-2" onClick={()=>{setName('Pedro')}}>Mudar para pedro</button>
+      <button className=" rounded-md bg-blue-700 border border-solid mt-5 p-2 mr-3" onClick={()=>{setName('João')}}>Mudar para joao</button>
+      <button className=" rounded-md bg-blue-700 border border-solid mt-5 p-2 ml-2" onClick={()=>{setAge(27)}}>Mudar para 27 anos</button>
+      <button className=" rounded-md bg-blue-700 border border-solid mt-5 p-2 ml-2" onClick={()=>{setAge(47)}}>Mudar para 47 anos</button>
   </div>
+
   )
 }
 
