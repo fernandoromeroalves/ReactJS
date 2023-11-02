ab6b0fe8683a1c31e5b5207f0cf8201835fa438f
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-02 02:05:51 -0300

    Effect, Reducers e Context

---
+++ iniciando-um-projeto-com-next/react-projeto-effect-reducers-context/src/app/page.tsx
@@ -0,0 +1,19 @@
+// npm run dev, para start
+//"use client" para eventos de click
+"use client"
+
+
+
+const Page = () => {
+  
+
+  return ( 
+    <div className="">
+     
+
+  </div>
+  )
+}
+
+
+export default Page;
