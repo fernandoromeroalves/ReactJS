43616a5b15577a42d67cfffb3ba767c9ab0c9e77
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-17 16:30:03 -0300

    .

--- cleanup/src/app/page.tsx
+++ cleanup/src/app/page.tsx
@@ -1,9 +1,23 @@
 "use client"
 
+
+
+
+
+import { useState } from "react"
+
 export default function Home() {
+
+  const [show, setShow] = useState(false);
+
+
   return (
     <div className="">
-      .......
+      <button onClick={() => setShow(!show)}>Mostrar/Ocultar</button>
+
+      {show && </>}
     </div>
   )
 }
+
+
---
+++ cleanup/src/components/Square.tsx
@@ -0,0 +1,10 @@
+
+
+export const Square = () => {
+    return(
+        <div className="w-40 h-40 bg-white-300"></div>
+    )
+    
+}
+
+
