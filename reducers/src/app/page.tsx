2052a1d50e834d8239759cd04e2b1c0a4f05dbe5
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-12-04 18:58:24 -0300

    reducers

--- reducers/src/app/page.tsx
+++ reducers/src/app/page.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react"
+"use client"
+import { useReducer, useState } from "react"
+import { Item } from "@/types/item";
+import { listReducer } from "@/reducers/listReducer";
 
 
 export default function Home() {
-  const [list, setList] = useState([])
+  const [list, dispatch] = useReducer(listReducer, [])
+
+  const handleAddClick = ()=>{
+    dispatch({
+      type:'add',
+      payload: {
+        text:'novo item'
+      }
+    });
+  }
+
+  dispatch({
+    type: 'toggleDone',
+    payload:{id: 2}
+  });
+  dispatch({
+    type: 'editText',
+    payload:{id: 2, newText:'bla bla bla'}
+  });
 
   return (
     <div className=''>
-
+      <button onClick={handleAddClick}>Adicionar</button>
     </div>
   )
 }
