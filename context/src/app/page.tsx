74f5120bf3c47bde4789146af1528ad1c76807ac
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-12-21 19:36:40 -0300

    0

--- context/src/app/page.tsx
+++ context/src/app/page.tsx
@@ -1,7 +1,37 @@
 'use client'
 
-export default function Home() {
+const Title = ({label}:{label: string})=>{
   return (
-    <div>...</div>
+    <h1 className="text-4xl font-bold my-4">{label}</h1>
+  )
+  
+}
+
+const SubTitle = ({label}:{label: string}) => {
+  return(
+    <h2>{label}</h2>
+  )
+  
+}
+
+const Header = ({title, subtitle}: {title:string; subtitle:string}) => {
+  return (
+    <header>
+    <Title label={title}/>
+    <SubTitle label={subtitle}/>
+  </header>
+  )
+  
+}
+export default function Home() {
+  const pageInfo = {
+    title: 'titulo maroto',
+    subtitle: 'algo muito legal'
+  }
+
+  return (
+    <div className="container mx-auto">
+      <Header title={pageInfo.title} subtitle={pageInfo.subtitle}/>
+    </div>
   )
 }
